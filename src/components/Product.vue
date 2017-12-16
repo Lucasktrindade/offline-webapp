61ae0e76b658cd4b9b5e797ea4dfdda6e54dce3e
Lucas Trindade <lucasktrindade@gmail.com>
2017-12-16 10:16:36 -0200

    Adiciona Product  Component

---
+++ src/components/Product.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="product">
+    <h4 class="product--name">{{item.name}}</h4>
+    <div class="product--detail">
+      <div class="product--image">
+        <img v-bind:src="item.thumb" alt="" class="product--image--thumb">
+      </div>
+      <div class="product--text">
+        <p>{{item.description}}</p>
+        <p>R$ {{item.price}}</p>
+      </div>
+    </div>
+    <button type="button" name="button" @click="addToCart(item)" class="product--button">Adicionar ao Carrinho</button>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'product',
+    props: ['item']
+  }
+</script>
+<style scoped>
+  .product {
+    border: 1px solid #bdc3c7;
+    border-radius: 2px;
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    width: 250px;
+    max-width: 350px;
+    margin-bottom: .2em;
+    margin-left: .375em;
+    margin-right: .375em;
+    margin-top: .2em;
+  }
+  .product--detail {
+      -webkit-box-align: center;
+      -webkit-align-items: center;
+      -ms-flex-align: center;
+      align-items: center;
+      display: -webkit-box;
+      display: -webkit-flex;
+      display: -ms-flexbox;
+      display: flex;
+      -webkit-box-orient: horizontal;
+      -webkit-box-direction: normal;
+      -webkit-flex-flow: row wrap;
+      -ms-flex-flow: row wrap;
+              flex-flow: row wrap;
+      -webkit-box-flex: 1;
+      -webkit-flex-grow: 1;
+      -ms-flex-positive: 1;
+              flex-grow: 1;
+      margin-bottom: .750em;
+      margin-top: .750em;
+      padding: 0em .375em;
+    }
+    .product--button {
+      background-color: #dddddd;
+      border: none;
+      border-top: 1px solid #bdc3c7;
+      cursor: pointer;
+      padding: .5em 0em;
+      width: 100%;
+    }
+    .product--button:hover, .product--button:focus {
+        background-color:darken(#dddddd, 10%);
+      }
+    .product--image {
+      -webkit-flex-basis:50%;
+      -ms-flex-preferred-size:50%;
+      flex-basis:50%;
+    }
+    .product--image--thumb {
+        border-radius: 5px;
+        box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+      }
+    .product--text {
+      flex-basis: 50%;
+      text-align: center;
+    }
+    .product--name {
+      display: block;
+      text-align: center;
+    }
+</style>
--- src/pages/Showcase.vue
+++ src/pages/Showcase.vue
@@ -1,26 +1,52 @@
 <template>
-    
+    <div>
+      <div class="product-list">
+        <product v-for="item in products" :key="item.id" :item="item"></product>
+      </div>
+    </div>
 </template>
 <style>
-
+    .product-list {
+      -webkit-align-content: center;
+      -ms-flex-line-pack: center;
+      align-content: center;
+      display: -webkit-box;
+      display: -webkit-flex;
+      display: -ms-flexbox;
+      display: flex;
+      -webkit-box-orient: horizontal;
+      -webkit-box-direction: normal;
+      -webkit-flex-flow: row wrap;
+      -ms-flex-flow: row wrap;
+              flex-flow: row wrap;
+      -webkit-box-pack: center;
+      -webkit-justify-content: center;
+      -ms-flex-pack: center;
+              justify-content: center;
+    }
 </style>
 <script>
 import FirebaseService from '@/services/FirebaseService'
+import Product from '@/components/Product'
 
 export default {
   name: 'Showcase',
+  components: {
+    Product
+  },
   data () {
     return {
       title: 'Produtos',
-      firebaseService: new FirebaseService()
+      firebaseService: new FirebaseService(),
+      products: []
     }
   },
   mounted () {
-    this.firebaseService.getFirestore()
-      .collection('products').get().then((querySnapshot) => {
-        console.log(querySnapshot)
-        querySnapshot.forEach((doc) => {
-          console.log(`${doc.id} => ${doc.data()}`)
+    const productsRef = this.firebaseService.getFirestore().collection('products')
+    productsRef.get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((value) => {
+          this.products.push(value.data())
         })
       })
   }
